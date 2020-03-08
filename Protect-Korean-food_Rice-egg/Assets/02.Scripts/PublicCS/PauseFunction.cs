a4b0bc18dd2f3efafa778e36aa7e335acd3999b5
YSJ0413 <didtmdwns456@naver.com>
2020-03-08 20:39:53 +0900

    Add Public CS
    Add PublicCS - PauseFunction.cs

---
+++ Protect-Korean-food_Rice-egg/Assets/02.Scripts/PublicCS/PauseFunction.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseFunction : MonoBehaviour
+{
+    public Sprite[] sprites;
+
+    private Image image;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+
+    private void Start()
+    {
+        Time.timeScale = 1;
+        image.sprite = (Time.timeScale == 1) ? sprites[0] : sprites[1];
+    }
+
+    public void OnClickPause()
+    {
+        if(Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+            image.sprite = sprites[1];
+        }
+        else
+        {
+            Time.timeScale = 1;
+            image.sprite = sprites[0];
+        }
+    }
+}
