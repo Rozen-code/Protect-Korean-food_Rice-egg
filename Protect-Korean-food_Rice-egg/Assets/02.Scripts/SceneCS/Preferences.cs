2372b0dc0d4c54d339df98f935c102c85ed82c18
GrowUp <didtmdwns456@naver.com>
2020-08-06 03:23:14 +0900

    TitlePng
    PngFile

---
+++ Protect-Korean-food_Rice-egg/Assets/02.Scripts/SceneCS/Preferences.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Preferences : MonoBehaviour
+{
+    bool isClick;
+    bool preferencesListSee;
+    float delayTime;
+    public float delayTimeMax;
+    public GameObject pList;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isClick = false;
+        preferencesListSee = false;
+        pList.GetComponent<Image>().fillAmount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isClick)
+        {
+            delayTime += Time.deltaTime;
+
+            pList.GetComponent<Image>().fillAmount
+            += (preferencesListSee) ? (Time.deltaTime / delayTimeMax) : (Time.deltaTime / delayTimeMax) * (-1);
+        }
+        
+        if (delayTime > delayTimeMax )
+            isClick = false;
+    }
+
+    public void PreferencesListView()
+    {
+        preferencesListSee = (preferencesListSee) ? false : true;
+        isClick = true;
+        delayTime = 0;
+    }
+
+    /* IEnumerator Test() 
+    {
+        yield return null;
+
+        while(delayTime < delayTimeMax)
+        {
+            delayTime += Time.deltaTime;
+
+            //.....
+
+
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+    } */
+}
---
+++ Protect-Korean-food_Rice-egg/Assets/02.Scripts/SceneCS/TouchStart.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchStart : MonoBehaviour
+{
+    Image image;
+    float delayTime;
+    public float delayTimeMax;
+
+    void Start()
+    {
+        image = this.gameObject.GetComponent<Image>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        delayTime += Time.deltaTime;
+        if (delayTime > delayTimeMax)
+        {
+            delayTime = 0; 
+            if(image.color.a == 1)
+            {
+                image.color = new Color(1, 1, 1, 0);
+            }else if(image.color.a == 0)
+            {
+                image.color = new Color(1, 1, 1, 1);
+            }
+        }
+    }
+}
