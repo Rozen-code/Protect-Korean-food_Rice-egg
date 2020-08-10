0bea408f1bcc821878448275718fdb8bf8313ae3
GrowUp <didtmdwns456@naver.com>
2020-08-10 17:30:43 +0900

    cs
    cscs

--- Protect-Korean-food_Rice-egg/Assets/02.Scripts/Noman.cs
+++ Protect-Korean-food_Rice-egg/Assets/02.Scripts/Noman.cs
@@ -7,7 +7,8 @@ public class Noman : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Strat
+        // Strat
+        // 희희희
     }
 
     // Update is called once per frame
