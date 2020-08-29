5de215e9d5bd307871ab4bf01967b56e8b16f582
Sio-Sniper <60175862+Sio-Sniper@users.noreply.github.com>
2020-08-29 23:10:06 +0900

    MiniGame Connect
    MiniGame Connect Ready

---
+++ Protect-Korean-food_Rice-egg/Assets/02.Scripts/MiniGameCS/MiniGameList.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MiniGameList : MonoBehaviour
+{
+    public Sprite[] miniGameList;
+    private int ListNumber = 0;
+
+    void Start()
+    {
+        ChangeMiniGame();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void MiniGameRight() {
+        if(ListNumber >= miniGameList.Length - 1) {
+            ListNumber = ListNumber % (miniGameList.Length - 1);
+        }
+        else {
+            ListNumber++;
+        }
+
+        ChangeMiniGame();
+    }
+
+    public void MiniGameLeft() {
+        if(ListNumber <= 0) {
+            ListNumber = miniGameList.Length - 1;
+        }
+        else {
+            ListNumber--;
+        }
+        
+        ChangeMiniGame();
+    }
+
+    void ChangeMiniGame() {
+        this.GetComponent<Image>().sprite = miniGameList[ListNumber];
+    }
+}
